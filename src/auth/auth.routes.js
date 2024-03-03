179b7be2da13e7d07b611066eaad9c320638120d
Pedro Lopez <plopez-2019280@kinal.edu.gt>
2024-03-03 00:47:14 -0600

    Subiendo actualización de archivo

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,19 +1,22 @@
 import { Router } from "express";
 import { check } from "express-validator";
-
+import validator from 'validator';
 import { login } from "./auth.controller.js";
 import { validarCampos } from "../middlewares/validarCampos.js";
 
 const router = Router();
 
 router.post(
-  "/login",
-  [
-    check("email", "This is not a valid email").isEmail(),
-    check("password", "The password is required").not().isEmpty(),
-    validarCampos,
-  ],
-  login
+  '/login', [
+      check('email')
+      .notEmpty().withMessage('The Email is required')
+      .custom((value, { req }) => {
+          if (!validator.isEmail(value)) {
+          }
+          return true;
+      }),
+      check('password', 'The Password is required').notEmpty(),
+      validarCampos,
+  ], login
 );
-
 export default router;
